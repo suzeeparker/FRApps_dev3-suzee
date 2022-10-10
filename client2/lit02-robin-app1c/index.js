5cdd644eab73890f02f4d52be91fc48f28e1e997
robinmattern <robin.mattern@gmail.com>
2022-10-09 20:28:56 -0400

    Added two more Javascript apps

---
+++ client2/lit02-robin-app1c/index.js
@@ -0,0 +1,1 @@
+export { Lit02RobinApp1c } from './src/Lit02RobinApp1c.js';
---
+++ client2/lit02-robin-app1c/lit02-robin-app1c.js
@@ -0,0 +1,3 @@
+import { Lit02RobinApp1c } from './src/Lit02RobinApp1c.js';
+
+window.customElements.define('lit02-robin-app1c', Lit02RobinApp1c);
